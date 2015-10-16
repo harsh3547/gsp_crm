9e1c102fba8da44e47b760ce733b217369f0d242
Harsh Jain <harsh.jain@jginfosystems.com>
2015-10-16 22:47:39 +0530

    override action_wait instead of action_button_confirm

--- sale_order.py
+++ sale_order.py
@@ -35,32 +35,33 @@ class sale_order(osv.osv):
               }
    
     # overriding the action_button_confirm(confirm sale order) method to run the 'mark won' button in the related opportunity
-    def action_button_confirm(self, cr, uid, ids, context=None):
-        print "------in action_button_confirm----in gsp_crm=-=-=-=-=-"
-        result =  super(sale_order,self).action_button_confirm(cr, uid, ids, context)
-        try:
-            origin=self.browse(cr,uid,ids[0]).origin
-            if origin:
-                origin_id=(origin.split(' '))[1]
-                origin_id=int(origin_id)
-                # to check if the opportunity has this sale order in ref
-                crm_obj=self.pool.get('crm.lead')
-                i=crm_obj.browse(cr,uid,origin_id).ref.id
-                stage_id=crm_obj.browse(cr,uid,origin_id).stage_id
-                #print "3333333333333333"
-                #print "origin id ===========",origin_id
-                #print "origin id ===========",type(origin_id)
-                #print "i ===================",i
-                #print "i ===================",type(i)
-                #print "ids of sale orderi in oportunity",ids[0]
-                if ids[0]==i and stage_id.probability != 100.00:
-                    #print 'opportunity has this sales order'
-                    context_new={'stage_type': 'opportunity', 'default_type': 'opportunity', 'default_user_id': uid, 'needaction_menu_ref': 'sale.menu_sale_quotations','uid':uid,'via_button':False}
-                    # via button to distinguish when called from button or another method
-                    crm_obj.case_mark_won(cr, uid,[origin_id],context_new)
-        except:
-            raise
-            print "error =================================== error in gsp/sale_order.py/def action_button_confirm"        
+    def action_wait(self, cr, uid, ids, context=None):
+        print "------in action_wait----in gsp_crm=-=-=-=-=-"
+        result =  super(sale_order,self).action_wait(cr, uid, ids, context)
+        for rec in self.browse(cr,uid,ids,context):
+            try:
+                origin=rec.origin
+                if origin:
+                    origin_id=(origin.split(' '))[1]
+                    origin_id=int(origin_id)
+                    # to check if the opportunity has this sale order in ref
+                    crm_obj=self.pool.get('crm.lead')
+                    i=crm_obj.browse(cr,uid,origin_id).ref.id
+                    stage_id=crm_obj.browse(cr,uid,origin_id).stage_id
+                    #print "3333333333333333"
+                    #print "origin id ===========",origin_id
+                    #print "origin id ===========",type(origin_id)
+                    #print "i ===================",i
+                    #print "i ===================",type(i)
+                    #print "ids of sale orderi in oportunity",ids[0]
+                    if rec.id==i and stage_id.probability != 100.00:
+                        #print 'opportunity has this sales order'
+                        context_new={'stage_type': 'opportunity', 'default_type': 'opportunity', 'default_user_id': uid, 'needaction_menu_ref': 'sale.menu_sale_quotations','uid':uid,'via_button':False}
+                        # via button to distinguish when called from button or another method
+                        crm_obj.case_mark_won(cr, uid,[origin_id],context_new)
+            except:
+                raise
+                print "error =================================== error in gsp/sale_order.py/def action_wait in sale order id =",rec.id        
         return result
         
     # overriding the action_cancel ( cancel sales order) method to run the 'mark won' button in the related opportunity
